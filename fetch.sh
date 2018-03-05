cb50058c18ff7303e213019cbfc0feb61349b047
dyu <david.yu.ftw@gmail.com>
2018-03-05 17:21:39 +0800

    check arg

--- fetch.sh
+++ fetch.sh
@@ -18,6 +18,44 @@ UWS_GZ=target/uws-$UWS_BRANCH.tar.gz
 JSON_VERSION=2.1.1
 JSON_H=nlohmann-json/json.hpp
 
+fetch_gtest() {
+[ ! -e googletest/googletest ] && \
+    wget https://github.com/google/googletest/archive/release-$GTEST_VERSION.tar.gz -O $GTEST_GZ  && \
+    tar -xvzf $GTEST_GZ --strip-components=1 -C googletest
+}
+
+fetch_snappy() {
+# https://github.com/google/snappy/releases/download/$SNAPPY_VERSION/snappy-$SNAPPY_VERSION.tar.gz has a different source!
+[ ! -e snappy/snappy.h ] && \
+    wget https://github.com/google/snappy/archive/$SNAPPY_VERSION.tar.gz -O $SNAPPY_GZ && \
+    tar -xvzf $SNAPPY_GZ --strip-components=1 -C snappy
+}
+
+fetch_leveldb() {
+[ ! -e leveldb/include ] && \
+    wget https://github.com/google/leveldb/archive/v$LEVELDB_VERSION.tar.gz -O $LEVELDB_GZ && \
+    tar -xvzf $LEVELDB_GZ --strip-components=1 -C leveldb && \
+    patch leveldb/port/port.h < leveldb/gn-diff/leveldb_port.diff && \
+    patch leveldb/include/leveldb/slice.h < leveldb/gn-diff/leveldb_slice.diff
+}
+
+fetch_libuv() {
+[ ! -e libuv/include ] && \
+    wget https://github.com/libuv/libuv/archive/v$LIBUV_VERSION.tar.gz -O $LIBUV_GZ && \
+    tar -xvzf $LIBUV_GZ --strip-components=1 -C libuv
+}
+
+fetch_uws() {
+[ ! -e uWebSockets/src ] && \
+    wget http://github.com/dyu/uWebSockets/archive/$UWS_BRANCH.tar.gz -O $UWS_GZ && \
+    tar -xvzf $UWS_GZ --strip-components=1 -C uWebSockets
+}
+
+fetch_json() {
+[ ! -e $JSON_H ] && \
+    wget https://github.com/nlohmann/json/releases/download/v$JSON_VERSION/json.hpp -O $JSON_H
+}
+
 CURRENT_DIR=$PWD
 # locate
 if [ ! -n "$BASH_SOURCE" ]; then
@@ -34,29 +72,40 @@ cd $SCRIPT_DIR
 
 mkdir -p target
 
-[ ! -e googletest/googletest ] && \
-    wget https://github.com/google/googletest/archive/release-$GTEST_VERSION.tar.gz -O $GTEST_GZ  && \
-    tar -xvzf $GTEST_GZ --strip-components=1 -C googletest
+case "$1" in
+    gtest)
+    fetch_gtest
+    ;;
+    
+    snappy)
+    fetch_snappy
+    ;;
+    
+    leveldb)
+    fetch_leveldb
+    ;;
+    
+    libuv)
+    fetch_libuv
+    ;;
+    
+    uws)
+    fetch_uws
+    ;;
+    
+    json)
+    fetch_json
+    ;;
 
-# https://github.com/google/snappy/releases/download/$SNAPPY_VERSION/snappy-$SNAPPY_VERSION.tar.gz has a different source!
-[ ! -e snappy/snappy.h ] && \
-    wget https://github.com/google/snappy/archive/$SNAPPY_VERSION.tar.gz -O $SNAPPY_GZ && \
-    tar -xvzf $SNAPPY_GZ --strip-components=1 -C snappy
+    *)
+    fetch_gtest
+    fetch_snappy
+    fetch_leveldb
+    fetch_libuv
+    fetch_uws
+    fetch_json
+    ;;
+esac
 
-[ ! -e leveldb/include ] && \
-    wget https://github.com/google/leveldb/archive/v$LEVELDB_VERSION.tar.gz -O $LEVELDB_GZ && \
-    tar -xvzf $LEVELDB_GZ --strip-components=1 -C leveldb && \
-    patch leveldb/port/port.h < leveldb/gn-diff/leveldb_port.diff && \
-    patch leveldb/include/leveldb/slice.h < leveldb/gn-diff/leveldb_slice.diff
 
-[ ! -e libuv/include ] && \
-    wget https://github.com/libuv/libuv/archive/v$LIBUV_VERSION.tar.gz -O $LIBUV_GZ && \
-    tar -xvzf $LIBUV_GZ --strip-components=1 -C libuv
-
-[ ! -e uWebSockets/src ] && \
-    wget http://github.com/dyu/uWebSockets/archive/$UWS_BRANCH.tar.gz -O $UWS_GZ && \
-    tar -xvzf $UWS_GZ --strip-components=1 -C uWebSockets
-
-[ ! -e $JSON_H ] && \
-    wget https://github.com/nlohmann/json/releases/download/v$JSON_VERSION/json.hpp -O $JSON_H
 
